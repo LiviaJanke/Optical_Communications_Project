e7ba9f9f750d1cf4479e7aee9ec50104b8e5a328
Liv <eichmliv12@gmail.com>
2023-11-28 16:58:04 +0000

    solving for modes 0 - 3

--- optical_fibres_liv.py
+++ optical_fibres_liv.py
@@ -91,7 +91,130 @@ beta_val_m0 = beta_test_array_valid[index_vals[0]]
 
 
 # Repeating for m = 1:
+
+lhs_m1 = jv(2, (p_vals * a)) / (p_vals * a * jv(1, (p_vals * a)))
+
+rhs_m1 = - kv(2, (q_vals * a)) / (q_vals * a * kv(1, (q_vals * a)))
+
+index_vals = []
+func_vals = []
+
+for i in np.arange(0, len(beta_test_array_valid)):
     
+    lhs = lhs_m1[i]
+    
+    rhs = rhs_m1[i]
+    
+    if np.abs(lhs - rhs) < 0.0001:
+        
+        index_vals.append(i)
+        
+        func_vals.append(lhs)
+        
+
+
+beta_val_m1 = beta_test_array_valid[index_vals[0]]
+
+
+#%%
+
+
+# Repeating for m = 2:
+
+lhs_m2 = jv(3, (p_vals * a)) / (p_vals * a * jv(2, (p_vals * a)))
+
+rhs_m2 = - kv(3, (q_vals * a)) / (q_vals * a * kv(2, (q_vals * a)))
+
+index_vals = []
+func_vals = []
+
+for i in np.arange(0, len(beta_test_array_valid)):
+    
+    lhs = lhs_m2[i]
+    
+    rhs = rhs_m2[i]
+    
+    if np.abs(lhs - rhs) < 0.001:
+        
+        index_vals.append(i)
+        
+        func_vals.append(lhs)
+        
+
+
+beta_val_m2 = beta_test_array_valid[index_vals[0]]
+
+#%%
+
+# Repeating for m = 3:
+
+lhs_m3 = jv(4, (p_vals * a)) / (p_vals * a * jv(3, (p_vals * a)))
+
+rhs_m3 = - kv(4, (q_vals * a)) / (q_vals * a * kv(3, (q_vals * a)))
+
+index_vals = []
+func_vals = []
+
+for i in np.arange(0, len(beta_test_array_valid)):
+    
+    lhs = lhs_m3[i]
+    
+    rhs = rhs_m3[i]
+    
+    if np.abs(lhs - rhs) < 0.001:
+        
+        index_vals.append(i)
+        
+        func_vals.append(lhs)
+        
+
+
+beta_val_m3 = beta_test_array_valid[index_vals[0]]
+
+#%%
+
+# Repeating for m = 4:
+
+lhs_m4 = jv(5, (p_vals * a)) / (p_vals * a * jv(4, (p_vals * a)))
+
+rhs_m4 = - kv(5, (q_vals * a)) / (q_vals * a * kv(4, (q_vals * a)))
+
+index_vals = []
+func_vals = []
+
+for i in np.arange(0, len(beta_test_array_valid)):
+    
+    lhs = lhs_m4[i]
+    
+    rhs = rhs_m4[i]
+    
+    if np.abs(lhs - rhs) < 1:
+        
+        index_vals.append(i)
+        
+        func_vals.append(lhs)
+        
+
+
+beta_val_m4 = beta_test_array_valid[index_vals[0]]
+
+plt.plot(beta_test_array_valid[37990:99997], lhs_m4[37990:99997])
+plt.plot(beta_test_array_valid[37990:99997], rhs_m4[37990:99997])
+plt.show()
+
+
+plt.plot((p_vals * a)[37990:99997], lhs_m0[37990:99997])
+plt.plot((q_vals * a)[37990:99997], rhs_m0[37990:99997])
+plt.show()
+
+
+# Mode 4 is not included; no valid solutions
+
+#%%
+
+# find propagation constant and effective index for each mode
+
+
 
 
 
